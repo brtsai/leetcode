a12b48fc3f00ff62a56475e9382c7d8fbf2cc3dd
brtsai <bryan@bryantsaiwh.com>
2018-05-10 19:38:07 -0700

    add sln

---
+++ DeleteDuplicateEmails/DeleteDuplicateEmails.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+
+DELETE
+FROM
+    Person
+WHERE
+    Person.Id
+IN 
+(
+    SELECT tmp.Id FROM (
+        SELECT 
+            Id
+        FROM 
+            Person
+        WHERE 
+            Id
+        NOT IN
+            (SELECT tmp2.Id FROM (
+                SELECT
+                    MIN(Person.Id) as Id
+                FROM
+                    Person
+                GROUP BY
+                    Person.Email
+            ) tmp2)
+    ) tmp
+)
+ 
